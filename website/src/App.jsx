07b314e1ea310f3f2c457b9acd06b99ff71ded57
dustin duong <ddustinduong@gmail.com>
2024-09-19 22:18:55 -0700

    fully converted
    html fully converted to javascript

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 
       {/* Hamburger Navigation */}
       <nav id="hamburger-nav">
-        <div className="logo">Puzzle</div>
+        <div className="logo">Productivity Webevent</div>
         <div className="hamburger-menu">
           <div className={`hamburger-icon ${menuOpen ? "open" : ""}`} onClick={toggleMenu}>
             <span></span>
@@ -54,17 +54,21 @@ function App() {
           <img src= {ProductivityImage} alt="productivityimage" />
         </div>
         <div className="section_text">
-          <p className="section_text_p1">
-            Welcome to the Theta Tau Productivity Webevent. Whether you are someone who works to live or lives to work, work is a big part of our lives, especially with the beginning of the academic year. Below is a sparknotes-style document of Feel Good Productivity by Ali Abdaal and Deep Work by Cal Newport. Topics of productivity, procrastination, and burnout are covered while strategies and tips are recommended for you to try out.
-          </p>
+        <p className="section_text_p1">
+            Whether you are someone who works to live or lives to work, work is a big part of our lives, especially with the beginning of the academic year. Below is a sparknotes-style document on 
+            <a href="https://www.feelgoodproductivity.com/" target="_blank"> Feel Good Productivity</a> by Ali Abdaal and 
+            <a href="https://calnewport.com/deep-work-rules-for-focused-success-in-a-distracted-world/" target="_blank"> Deep Work</a> by Cal Newport. 
+          Both offer insights and dispel misconceptions about productivity, procrastination, and burnout. Some strategies and tips are included to try out.
+        </p>
           <a href="https://docs.google.com/document/d/1lfa_W_QzQktXelUZyjjqk3AHtK7tkeRM_5U_R4xD6Qo/edit?usp=sharing" className="button-link" target="_blank" rel="noopener noreferrer">
             Link to doc
           </a>
           <p className="section_text_p2">
-            Here's a little puzzle to challenge your productivity. First person to decode the crypt wins x. Keys are the key.
+            Here's a little puzzle to challenge your productivity. First person to decode the crypt wins x.
           </p>
         </div>
       </section>
+      <hr></hr>
 
       {/* Additional sections can go here */}
       {/* For example, a section for the puzzle piece: */}
@@ -87,7 +91,69 @@ function App() {
           I  P  E  H  H  N  N  A  S  K <br />
         </p>
       </section>
+      <hr></hr>
+      
+      <section id="1">
+        <ol class="section_text_piece1a">
+            <li>seal on ball</li>
+            <li>key</li>
+            <li>popsicle stick</li>
+            <li>pair of black arrows</li>
+            <li>snake eyes</li>
+            <li>uncaged bird</li>
+            <li>lighthouse</li>
+            <li>sailboat</li>
+            <li>rubber band</li>
+            <li>expert</li>
+            <li>lion</li>
+            <li>lock</li>
+        </ol>
+      </section>
+      <hr></hr>
+      
+      {/* Section 2 */}
+      <section id="2">
+        <p className="section_text_piece2a">
+          nine tails in the night <br />
+          eight black shadows, one moon white <br />
+          they dance, fearless fight <br />
+          <br />
+          the number of odd ones out make it right
+        </p>
+      </section>
+      <hr></hr>
+
+      {/* Crypt Section */}
+      <section id="crypt">
+        <p className="section_text_crypt1">
+          C zkyjvvgn jar ecd r adufmlv az xwztgguf evswczi niyhs by 
+          wydjxkzy jmsgxy hmhsnpee sbbol lug ap h kpwlybz-jduyg. 
+          Ov uguyw oehy pghijq lvdl xtyy hvi fas ynugt, vgv dycf 
+          hr mymqx mpk wyalrw as dymeo kfw bhgjl: "Mb igsc, gl's 
+          zhyi xcwgsp rzel'kl ran eylvr -- Yobw vrxs rqy xpwea yvsxm fq lrr!" 
+          Oafg'a lq quul km kal moik qqtl lljicx yefbqt ayyf wubui mhp iyznw? <br />
+          <br />
+          Pda hwng eo w owrknu xenz, xqp zebbeyqhp pk ywpyd. 
+          Ej pda beahz, pda dqjpan lhwyao w iennkn kj w opwjz. 
+          Pda hwng hwjzo ej bnkjp kb pda chwoo, opalo xwyg wjz bknpd, 
+          ajpnwjyaz xu epo ksj ikrejc eiwca wjz xu eiepwpera iwpejc zwjya
+          ep oaao lanbkniaz xabkna epo auao. Duljkpevaz, pda xenz hkoao
+          whh oajoa kb epo oqnnkqjzejco, qjpeh pda dqjpan'o jap pnwlo ep 
+          wcwejop pda iennkn. <br />
+          <br />
+          Weq lbkjev egvr vrrugkrt mno owchf. Mye izqrg afrvm yiam neot, 
+          dyg mye umcv uhish ecw ytqy. Wpg qrg sejip xb izlh bji exrr
+          kwtwr'l codl qr gav fuwpx uhish; ejia myeb pch gkrnvngvexu 
+          iw inp, gav rhit lbkje iwwrq bk edaa kbbeg, dvf lr lrig bq 
+          xux wrrvv lbkje: "Wwkp ngu szmcx! Gav mrzg cbn krb, bji zhie 
+          bww lnov tr awjsxi." Wkmp xuxp rhielrw khh bczrke, tkm qaaxi sdqf; 
+          "Aur jhrcnh V yfdgmt xjh youagw javn L kcver rlo wp sax? Z hdl
+          digmvr jqxi gav oqm cpy mye iwqh vm naqbu, eaw tuw bji gaiodb
+          qj gav owpgv; nm cedav M farlo pczr mye kqfi." Ngu sr pg hvw.
+        </p>
+      </section>
     </div>
+      
   );
 }
 
